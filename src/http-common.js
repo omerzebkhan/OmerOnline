601e2eee54ad5cc14254563189800b7aeb412854
Omer Theeb <m002571@corp.mtc.com.kw>
2021-12-29 23:06:16 +0500

    online store40

--- src/http-common.js
+++ src/http-common.js
@@ -3,11 +3,11 @@ import axios from "axios";
 
 export default axios.create({
   //local system test
-  baseURL: "http://localhost:8080/online/",
+  //baseURL: "http://localhost:8080/online/",
   //Heruko local test 
   //baseURL: "http://localhost:5000/online/",
   //Heruko remote test
- //baseURL :"https://damp-brushlands-21463.herokuapp.com/online/",
+ baseURL :"https://damp-brushlands-21463.herokuapp.com/online/",
 
   headers: {
     "Content-type": "application/json"
